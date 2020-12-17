078f74e89576619dcfc2fc6aaa9a64eb2485896a
Garrett Brown <garrettpaulbrown@gmail.com>
2020-12-16 21:57:50 -0800

    Create basic console write line.

---
+++ Bakery.Solution/Bakery/Model/Bakery.cs
@@ -0,0 +1,11 @@
+using System;
+using System.Text.RegularExpressions;
+using System.Collections.Generic;
+
+namespace Bakery.Models
+{
+    public class Bakery
+    {
+        //public
+    }
+}
---
+++ Bakery.Solution/BakeryTests/ModelTests/BakeryTests.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using Bakery.Models;
+using System;
+
+namespace Bakery.TestTool
+{
+    [TestClass]
+    public class BakeryTests
+    {
+        
+    }
+}
